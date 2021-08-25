899e437849f70d18db93ae20dee038242ef577fa
baokim1802 <baokim.htd@gmail.com>
2021-08-24 21:03:14 -0700

    test2

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -20,7 +20,7 @@ ShowScreen.navigationOptions = ({ navigation }) => {
     return {
         headerRight: () => (
             <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
-                <EvilIcons name="pencil" size={35} />    
+                <EvilIcons name="pencil" size={35} />      
             </TouchableOpacity>
         ),
     };
